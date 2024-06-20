5cd03a7c1225b62bd249e1088a46488e2c3c7003
Baptiste Viala <b.viala@groupeonepoint.com>
2024-06-20 10:44:28 +1000

    bug repro

--- Program.cs
+++ Program.cs
@@ -14,17 +14,17 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
     MSAL Authentication service support
     https://learn.microsoft.com/en-us/aspnet/core/blazor/security/webassembly/hosted-with-microsoft-entra-id?view=aspnetcore-7.0#authentication-service-support-1
 */
-builder.Services.AddHttpClient("Juice.PrivateAPI", client => 
+builder.Services.AddHttpClient("TEST", client => 
         client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
-    .CreateClient("Juice.PrivateAPI"));
+    .CreateClient("TEST"));
 
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("api://624db26f-4c8f-43b9-87c7-86b71270aea6/API.Access");
+    // options.ProviderOptions.DefaultAccessTokenScopes.Add("api://{{ API app reg ID }}/API.Access");
 });
 
 
